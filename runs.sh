0cafb03fcf1400bb7139a672438c8892bd2f3464
Florian Wechsung <f.wechsung@gmail.com>
2022-04-13 10:52:23 -0400

    rename

--- runs.sh
+++ runs.sh
@@ -1,6 +1,6 @@
 for MODE in 0 1 2; do
     for OMP in 1 8 96; do
         export OMP_NUM_THREADS=$OMP
-        python3 performance.py --MODE $MODE
+        python3 timing.py --MODE $MODE
     done
 done
